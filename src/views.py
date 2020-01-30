8d02f4ae33a5917e41af1ef6b275217b340ef00e
Abhinav-data <abhinavv1701@gmail.com>
2020-01-30 16:58:04 +0530

    created update section

--- src/views.py
+++ src/views.py
@@ -1,16 +1,25 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render,get_object_or_404
 from .models import BlogPost
-
+from .forms import BlogPostModelForm
 
 def blog_post_list_view(request):
 	qs=BlogPost.objects.all()
 	context={'object_list':qs}
 	return render(request,"blog/list.html",context)
 
-
+@staff_member_required
+#@login_required
 def blog_post_create_view(request):
-	context={'form':None}
-	return render(request,"blog/create.html",context)
+	form=BlogPostModelForm(request.POST or None)
+	if(form.is_valid()):
+		obj=form.save(commit=False)
+		obj.user=request.user
+		obj.save()
+		form=BlogPostModelForm()
+	context={'form':form}
+	return render(request,"form.html",context)
 
 def blog_post_detail_view(request,slug):
 	obj=get_object_or_404(BlogPost,slug=slug)
@@ -19,8 +28,12 @@ def blog_post_detail_view(request,slug):
 
 def blog_post_update_view(request,slug):
 	obj=get_object_or_404(BlogPost,slug=slug)
-	context={"object":obj}
-	return render(request,"blog/update.html",context)
+	form=BlogPostModelForm(request.POST or None,instance=obj)
+	print(form.is_valid())
+	if form.is_valid():
+		form.save()
+	context={'form':form,'title':f"Update {obj.title}"}
+	return render(request,"form.html",context)
 
 def blog_post_delete_view(request,slug):
 	obj=get_object_or_404(BlogPost,slug=slug)
